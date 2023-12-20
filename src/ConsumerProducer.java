cc958c978ef9ceea804f8f7318ab33885d0671cc
William Denoyer <denoyer.william@gmail.com>
2023-12-20 18:25:47 +0100

    Adding ProducerBehaviour

--- src/ConsumerProducer.java
+++ src/ConsumerProducer.java
@@ -7,13 +7,18 @@ public class ConsumerProducer extends Agent {
 
     private float priceProducedMerchandise = 1;
     private int stockProducedMerchandise = 0;
-    private int maxStockProducedMerchandise;
+    private int maxStockProducedMerchandise = 500; // TODO
+
+    private float productionSpeed;
 
     private int stockConsumedMerchandise = 0;
     private float money = 10;
     private float satisfaction = 1;
 
     protected void setup() {
+        // TODO : Handle production speed
+        productionSpeed = 3;
+
         System.out.println("Hello World (not new!...)!");
         System.out.println("My name is " + getAID().getName());
         System.out.println("My local name is " + getLocalName());
@@ -35,8 +40,30 @@ public class ConsumerProducer extends Agent {
 
         System.out.println("Consumed Merchandise is " + consumedMerchandise);
         System.out.println("Produced Merchandise is " + producedMerchandise);
+
+        addBehaviour(new ProducerBehaviour(this));
     }
 
     private void cloneAgent() {}
+
+    public boolean isSpaceInProducedStock() {
+        return stockProducedMerchandise < maxStockProducedMerchandise;
+    }
+
+    public void addProducedMerchandise() {
+        stockProducedMerchandise++;
+    }
+
+    public Merchandise getConsumedMerchandise() {
+        return consumedMerchandise;
+    }
+
+    public Merchandise getProducedMerchandise() {
+        return producedMerchandise;
+    }
+
+    public float getProductionSpeed() {
+        return productionSpeed;
+    }
 
 }
